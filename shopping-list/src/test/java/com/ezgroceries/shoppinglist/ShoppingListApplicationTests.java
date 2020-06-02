d3f38c2e963d8772c70372073d062fc80eeced76
vlemincxn <66299266+vlemincxn@users.noreply.github.com>
2020-06-03 00:17:06 +0200

    https://github.com/vlemincxn/cloud-native-practical/tree/master/lab-02-rest-api#shopping-list-api

--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/ShoppingListApplicationTests.java
@@ -1,13 +1,32 @@
 package com.ezgroceries.shoppinglist;
 
+import com.ezgroceries.shoppinglist.web.ShoppingListController;
+import com.ezgroceries.shoppinglist.web.ShoppingListResource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ShoppingListApplicationTests {
+	private ShoppingListController controller;
+
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCreateShoppingList(){
+		ShoppingListResource newShoppingList = new ShoppingListResource("Stephanie's Birthday");
+
+		HttpEntity<?> result = controller.createShoppinglist(newShoppingList);
+		assertNotNull(result);
 	}
 
 }
