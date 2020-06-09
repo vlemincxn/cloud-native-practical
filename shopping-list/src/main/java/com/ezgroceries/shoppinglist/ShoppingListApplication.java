4f5551d2b348e659783076b196b4e7f9f383b1a1
vlemincxn <66299266+vlemincxn@users.noreply.github.com>
2020-06-09 14:43:20 +0200

    lab04

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -2,6 +2,7 @@ package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableFeignClients
 @SpringBootApplication
 public class ShoppingListApplication {
 
---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/clients/CocktailDBResponse.java
@@ -0,0 +1,27 @@
+package com.ezgroceries.shoppinglist.clients;
+
+import java.util.List;
+
+public class CocktailDBResponse {
+    private List<DrinkResource> drinks;
+
+    public List<DrinkResource> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<DrinkResource> drinks) {
+        this.drinks = drinks;
+    }
+
+    public static class DrinkResource {
+        private String idDrink;
+        private String strDrink;
+        private String strGlass;
+        private String strInstructions;
+        private String strDrinkThumb;
+        private String strIngredient1;
+        private String strIngredient2;
+        private String strIngredient3;
+
+    }
+}
